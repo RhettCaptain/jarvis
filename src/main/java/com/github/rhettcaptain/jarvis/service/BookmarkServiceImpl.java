244f2935603f64470ce3a826d309318ebd14ac2d
hong_yu <hong_yu@worksap.co.jp>
2019-08-31 14:48:56 +0800

    refs vue ok

--- src/main/java/com/github/rhettcaptain/jarvis/service/BookmarkServiceImpl.java
+++ src/main/java/com/github/rhettcaptain/jarvis/service/BookmarkServiceImpl.java
@@ -12,7 +12,7 @@ import com.github.rhettcaptain.jarvis.vo.BookmarkVo;
 public class BookmarkServiceImpl implements BookmarkService {
 	public List<BookmarkVo> getAllBookmark() {
 		List<BookmarkVo> bookmarks = new ArrayList<BookmarkVo>();
-		BookmarkVo bm1 = BookmarkVo.builder().title("bm1").tags(Arrays.asList("a")).build();
+		BookmarkVo bm1 = BookmarkVo.builder().title("bm1").tags(Arrays.asList("a","d")).build();
 		BookmarkVo bm2 = BookmarkVo.builder().title("bm2").tags(Arrays.asList("b")).build();
 		bookmarks.add(bm1);
 		bookmarks.add(bm2);
