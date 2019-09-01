3ad694eb3acc363579506ae594eb51d3793f0cb7
hong_yu <hong_yu@worksap.co.jp>
2019-09-01 20:43:08 +0800

    refs fix regex

--- src/main/java/com/github/rhettcaptain/jarvis/service/BookmarkServiceImpl.java
+++ src/main/java/com/github/rhettcaptain/jarvis/service/BookmarkServiceImpl.java
@@ -24,7 +24,7 @@ public class BookmarkServiceImpl implements BookmarkService {
     }
 
     public List<BookmarkVo> getBookmarkWithKeywords(String keywords) {
-        final List<String> keywordsList = Arrays.asList(keywords.split(" *")).stream()
+        final List<String> keywordsList = Arrays.asList(keywords.split(" +")).stream()
                 .filter(str -> !StringUtils.isEmpty(str)).collect(
                         Collectors.toList());
         List<BookmarkVo> matchedBookmarks = getAllBookmark().stream()
